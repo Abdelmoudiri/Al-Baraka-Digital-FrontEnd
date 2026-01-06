fa52a4424c4d5185c48577647e935bc9b6a3ab89
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2026-01-06 14:35:21 +0100

    Configuration du Routing avec Lazy Loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./features/auth/auth-module')
+            .then(m => m.AuthModule)
+    },
+
+    {
+        path: 'client',
+        loadChildren: () => import('./features/client/client-module')
+            .then(m => m.ClientModule)
+    },
+    {
+        path: 'agent',
+        loadChildren: () => import('./features/agent/agent-module')
+            .then(m => m.AgentModule)
+    },
+
+    {
+        path: 'admin',
+        loadChildren: ()=>import('./features/admin/admin-module')
+            .then(m=>m.AdminModule)
+    },
+
+    {
+        path: '**',
+        redirectTo: 'auth/login'
+    }
+];
